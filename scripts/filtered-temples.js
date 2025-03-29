12dc0a37616c7a4017715137b62692f62df52f06
Your Name <you@example.com>
2025-03-29 14:26:46 -0400

    Add additional temple data to filtered temples JavaScript

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -22,7 +22,48 @@ document.addEventListener("DOMContentLoaded", () => {
             area: 96630,
             imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
         },
-        // Add more temple objects here...
+        {
+            templeName: "Provo City Center",
+            location: "Provo, Utah, United States",
+            dedicated: "2016, March, 20",
+            area: 85084,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/provo-city-center-temple/provo-city-center-temple-11074.jpg"
+        },
+        {
+            templeName: "St. George Utah",
+            location: "St. George, Utah, United States",
+            dedicated: "1877, April, 6",
+            area: 143969,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/st.-george-utah-temple/st.-george-utah-temple-36522.jpg"
+        },
+        {
+            templeName: "Quito Ecuador",
+            location: "Cumbayá, Quito, Ecuador",
+            dedicated: "2022, November, 20",
+            area: 36780,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/quito-ecuador-temple/quito-ecuador-temple-31201.jpg"
+        },
+        {
+            templeName: "Cochabamba Bolivia",
+            location: "Alto Queru Queru, Cochabamba, Bolivia",
+            dedicated: "2000, April, 30",
+            area: 35500,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/cochabamba-bolivia-temple/cochabamba-bolivia-temple-13721.jpg"
+        },
+        {
+            templeName: "Laie Hawaii",
+            location: "Laie, Hawaii, United States",
+            dedicated: "1919, November, 27",
+            area: 42100,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-3830.jpg"
+        },
+        {
+            templeName: "Logan Utah",
+            location: "Logan, Utah, United States",
+            dedicated: "1884, May, 17",
+            area: 119619,
+            imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/logan-utah-temple/logan-utah-temple-40550.jpg"
+        }
     ];
 
     // Display temples
