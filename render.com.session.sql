efbe4976afb2e8356c88d8ef13ee0880cb9c29b0
Your Name <you@example.com>
2025-09-16 14:59:14 -0400

    Add SQL session file with account management and inventory updates

---
+++ render.com.session.sql
@@ -0,0 +1,28 @@
+-- 1. Insert Tony Stark
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2. Update Tony Stark to Admin
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- 3. Delete Tony Stark
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+-- 4. Update GM Hummer description using REPLACE
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5. Inner join for Sport classification
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+-- 6. Update image paths to include /vehicles
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
